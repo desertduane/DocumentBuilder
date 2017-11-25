e324cce060fdf643ad175485476aad1ceb4acd22
duane rush <desertduane@yahoo.com>
2017-11-25 15:46:45 -0800

    document

---
+++ DocumentBuilder/Classes/Document.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentBuilder.Classes
+{
+    public class Document
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Pages { get; set; }
+        public List<string> Documents = new List<string>();
+
+        public void Write()
+        {
+            Console.WriteLine("Creating your documents");
+            foreach (string doc in Documents)
+            {
+                Console.WriteLine(doc);
+            }
+        }
+
+        public void Edit()
+        {
+            Console.WriteLine("Editing your docs");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Printing your docs");
+        }
+
+    }
+}
